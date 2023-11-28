09088fe17f216ca285579e508b6b7e152d373125
jasonoh22 <jasonoh22@naver.com>
2023-11-28 21:38:19 +0900

    feat : UserCreateForm

---
+++ src/main/java/com/ll/sbbmission/user/UserCreateForm.java
@@ -0,0 +1,23 @@
+package com.ll.sbbmission.user;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateForm {
+    @Size(min = 3, max = 25) // 검증 조건
+    @NotEmpty(message = "사용자ID는 필수항목입니다.")
+    private String username;
+
+    @NotEmpty(message = "비밀번호는 필수항목입니다.")
+    private String password1;
+
+    @NotEmpty(message = "비밀번호 확인은 필수항목입니다.")
+    private String password2;
+
+    @NotEmpty(message = "이메일은 필수항목입니다.")
+    @Email // 이메일 형식 확인
+    private String email;
+}
